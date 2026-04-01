396feaf08e4f53925b0a1f4bcc81ce4ec5227893
ChentAyoub <chentayoub1@gmail.com>
2026-04-01 05:59:50 +0100

    feat exams CRUD

--- school/subjects/urls.py
+++ school/subjects/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('edit/<str:subject_id>/', views.edit_subject, name='edit_subject'),
     path('delete/<str:subject_id>/', views.delete_subject, name='delete_subject'),
     path('edit-portal/', views.subject_edit_portal, name='subject_edit_portal'),
+    path('exams/', views.exam_list, name='exam_list'),
+    path('exams/add/', views.add_exam, name='add_exam'),
+    path('exams/edit/<str:exam_id>/', views.edit_exam, name='edit_exam'),
+    path('exams/delete/<str:exam_id>/', views.delete_exam, name='delete_exam'),
 ]
